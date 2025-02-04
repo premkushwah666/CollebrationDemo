f3df3bd493e2f9f386f7d4a8fc6abb87b0da2698
premkushwah666 <premkushwah666@gmail.com>
2025-02-04 14:35:43 +0530

    updated file Prem.java

--- Prem.java
+++ Prem.java
@@ -2,5 +2,6 @@ public class Prem {
     int prem;
     public static void myFun(String[] args) {
         System.out.println("this is prem");
+        System.out.println("this is next added line");
     }
 }
